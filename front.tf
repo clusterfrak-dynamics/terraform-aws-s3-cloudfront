ac58a207bea619b10695f6d9bfc27c2cc2e67e6c
Kevin Lefevre <lefevre.kevin@gmail.com>
2019-09-20 09:10:27 +0200

    fix user

--- front.tf
+++ front.tf
@@ -225,11 +225,11 @@ output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
-output "ecr_user_access_key_id" {
+output "s3_user_access_key_id" {
   value     = aws_iam_access_key.s3_front_user_key.id
 }
 
-output "ecr_user_secret_access_key" {
+output "s3_user_secret_access_key" {
   value     = aws_iam_access_key.s3_front_user_key.secret
   sensitive = true
 }
