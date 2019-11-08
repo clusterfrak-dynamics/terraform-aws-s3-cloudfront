b03018dac1d04ad3dd0fc39b855f47a35f4ff931
Kevin Lefevre <lefevre.kevin@gmail.com>
2019-11-08 14:21:42 +0100

    fix: remove dependency on data source
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- providers.tf
+++ providers.tf
@@ -12,8 +12,3 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 data "aws_caller_identity" "current" {}
-
-data "aws_route53_zone" "main" {
-  count = var.dns["use_route53"] ? 1 : 0
-  name  = var.dns["domain_name"]
-}
